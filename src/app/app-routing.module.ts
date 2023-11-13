9ba37f3af63b7ae63e57120f00b017284b357110
Dika1485 <ptiandika@gmail.com>
2023-11-13 10:39:37 +0700

    up first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,42 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { AutoLoginGuard } from './guards/auto-login.guard';
+import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
+{
+path: 'home',
+loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+},
+{
+path: '',
+redirectTo: 'login',
+pathMatch: 'full'
+// path: '',
+// loadChildren: () => import('./login/login.module').then( m =>
+// m.LoginPageModule),
+// canLoad: [AutoLoginGuard]
+},
+{
+path: 'login',
+loadChildren: () => import('./login/login.module').then( m =>
+m.LoginPageModule),
+canLoad: [AutoLoginGuard]
+},
+{
+path: 'dashboard',
+loadChildren: () => import('./dashboard/dashboard.module').then( m =>
+m.DashboardPageModule),
+canLoad: [AuthGuard] // Secure all child pages
+},
   {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
 ];
-
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+imports: [
+RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+],
+exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
